c12ca2793073cca6a0ec202097d3bcad108cbb09
Andrew Armstrong <a.j.armstrong78@gmail.com>
2019-01-14 20:50:28 +1300

    Added edit-article functionality, finished release 1.

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def dashboard():
     cur = mysql.connection.cursor()
 
     # Get articles
-    result = cur.execute("SELECT * FROM articles")
+    result = cur.execute("SELECT * FROM articles WHERE author = %s",[session['name']])
 
     articles = cur.fetchall()
 
@@ -217,7 +217,66 @@ def add_article():
 
     return render_template('add_article.html', form=form)
 
+# Edit Article
+@app.route ('/edit_article/<string:id>', methods=['GET','POST'])
+@is_logged_in
+def edit_article(id):
+    #create cursor
+    cur = mysql.connection.cursor()
 
+    #Get article by id
+    result = cur.execute("SELECT * FROM articles WHERE id = %s",[id])
+
+    article = cur.fetchone()
+
+    #Get form
+    form = ArticleForm(request.form)
+
+    #Populate article form fields
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        #create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute
+        cur.execute("UPDATE articles SET title=%s, body=%s WHERE id = %s",(request.form['title'], request.form['body'], id))
+
+        #Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('Article updated','success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_article.html', form=form)
+
+# Delete article
+@app.route('/delete_article/<string:id>', methods=["POST"])
+@is_logged_in
+def delete_article(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    #Execute
+    cur.execute("DELETE FROM articles where id = %s", [id])
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    #close connection
+    cur.close()
+
+    flash('Article deleted', 'success')
+
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key='hsieuhSIUEhf'
